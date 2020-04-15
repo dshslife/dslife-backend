46592f09896b8fc123ad34071bb54a9526eb8008
happycastle114 <41810556+happycastle114@users.noreply.github.com>
2020-04-15 23:27:45 +0900

    DB Setting
    DB Setting

--- framework/db.go
+++ framework/db.go
@@ -2,6 +2,7 @@ package framework
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -15,108 +16,170 @@ var (
 
 // InitDB db
 func InitDB() {
-	if d, err := sql.Open("sqlite3", "bot.db"); err == nil {
+	if d, err := sql.Open("sqlite3", "user.db"); err == nil {
 		db = d
 		rows, _ := db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='user'")
 		if !rows.Next() {
 			db.Exec(`
 			CREATE TABLE "user" (
-				"i" INTEGER NOT NULL AUTOINCREMENT,
+				"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"unique_id" TEXT NOT NULL,
 				"id" TEXT NOT NULL,
 				"password" TEXT NOT NULL,
 				"name" TEXT NOT NULL,
 				"number" TEXT NOT NULL,
 				"room" INTEGER NOT NULL,
-				"type" TEXT NOT NULL,
-				PRIMARY KEY("i")
+				"type" TEXT NOT NULL
 			)
 			`)
 		}
+		rows.Close()
 
 		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='token'")
 		if !rows.Next() {
 			db.Exec(`
 			CREATE TABLE "token" (
-				"i" INTEGER NOT NULL AUTOINCREMENT,
+				"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"token" TEXT NOT NULL,
 				"unique_id" TEXT NOT NULL,
 				"id" TEXT NOT NULL,
 				"name" TEXT NOT NULL,
 				"number" TEXT NOT NULL,
 				"room" INTEGER NOT NULL,
-				"type" TEXT NOT NULL,
-				PRIMARY KEY("i")
+				"type" TEXT NOT NULL
 			)
 			`)
 		}
+		rows.Close()
 
 		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='point'")
 		if !rows.Next() {
 			db.Exec(
 				`CREATE TABLE "point" (
-					"i" INTEGER NOT NULL AUTOINCREMENT,
+					"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 					"unique_id" TEXT NOT NULL,
 					"number" TEXT NOT NULL,
 					"name" TEXT NOT NULL,
 					"point" INTEGER NOT NULL,
-					"reason" TEXT NOT NULL,
-					PRIMARY KEY("i")
+					"reason" TEXT NOT NULL
 				)
 				`)
 		}
+		rows.Close()
 
 		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='meal'")
 		if !rows.Next() {
 			db.Exec(
 				`CREATE TABLE "meal" (
-					"i" INTEGER NOT NULL AUTOINCREMENT,
+					"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 					"date" DATE NOT NULL,
 					"type" INTEGER NOT NULL,
-					"menu" TEXT NOT NULL,
-					PRIMARY KEY("i")
+					"menu" TEXT NOT NULL
 				)
 				`)
 		}
+		rows.Close()
 
-		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='board'")
+		rows, err = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='board'")
 		if !rows.Next() {
-			db.Exec(
+			_, err = db.Exec(
 				`CREATE TABLE "board" (
-					"i" INTEGER NOT NULL AUTOINCREMENT,
+					"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 					"author" TEXT NOT NULL,
 					"type" TEXT NOT NULL,
+					"title" TEXT NOT NULL,
 					"content" TEXT NOT NULL,
-					"file" TEXT NOT NULL,
-					PRIMARY KEY("i")
+					"file" TEXT NOT NULL
 				)`)
+			if err != nil {
+				panic(err)
+			}
 		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		rows.Close()
 
 		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='alarm'")
 		if !rows.Next() {
-			db.Exec(
-				`CREATE TABLE "alarm" (
-					"i" INTEGER NOT NULL AUTOINCREMENT,
-					"name" TEXT NOT NULL,
-					PRIMARY KEY("i")
+			db.Exec(`CREATE TABLE "alarm" (
+					"i" INTEGER PRIMARY KEY AUTOINCREMENT,
+					"name" TEXT NOT NULL
 				)`)
 		}
+		rows.Close()
 
 		rows, _ = db.Query("SELECT name FROM sqlite_master WHERE type='table' AND name='declaration'")
 		if !rows.Next() {
-			db.Exec(
-				`CREATE TABLE "declaration" (
-					"i" INTEGER NOT NULL AUTOINCREMENT,
+			fmt.Println("MAKE SUCCESSFUL!")
+			db.Exec(`CREATE TABLE "declaration" (
+					"i" INTEGER PRIMARY KEY AUTOINCREMENT,
 					"author" TEXT NOT NULL,
-					"content" TEXT NOT NULL,
-					PRIMARY KEY("i")
+					"title" TEXT NOT NULL,
+					"content" TEXT NOT NULL
 					)`)
 		}
-
+		rows.Close()
+		fmt.Println("MAKE SUCCESSFUL!")
+	} else {
+		panic(err)
 	}
 }
 
-func insert(table string, args ...string) (sql.Result, error) {
+// Insert is insert
+func Insert(table string, args ...string) (sql.Result, error) {
+	fmt.Println(strings.Join(args, ", "))
 	res, err := db.Exec(fmt.Sprintf("INSERT INTO %s VALUE (, %s)", table, strings.Join(args, ", ")))
 	return res, err
+}
+
+// InsertPOST
+func InsertPOST(table string, author string, posttype string, title string, content string, jsondata string) {
+	statement, err := db.Prepare("INSERT INTO board(author, type, title, content, file) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		fmt.Println("First Error")
+		fmt.Println(err)
+	}
+	_, err = statement.Exec(author, posttype, title, content, jsondata)
+	defer statement.Close()
+
+	// return res, err
+}
+
+type FileUrl struct {
+	FILENAME string `json:"filename"`
+	URL      string `json:"fileurl"`
+}
+
+type PostInfo struct {
+	Author  string    `json:"author"`
+	Title   string    `json:"title"`
+	Content string    `json:"content"`
+	File    []FileUrl `json:"file"`
+}
+
+func GetPOST(id int) string {
+	rows, err := db.Query(`SELECT author, title, content, file FROM board WHERE i=?`, id)
+	if err != nil {
+		panic(err)
+	}
+	if rows.Next() {
+		var jsondata string
+		var author string
+		var title string
+		var content string
+		var files []FileUrl
+		rows.Scan(&author, &title, &content, &jsondata)
+		json.Unmarshal([]byte(jsondata), &files)
+		var postinfo = PostInfo{Author: author, Title: title, Content: content, File: files}
+		fmt.Println(postinfo)
+		data, err := json.Marshal(postinfo)
+		fmt.Println(string(data))
+		if err != nil {
+			panic(err)
+		}
+		return string(data)
+	} else {
+		return "nodata"
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"dslife-backend/framework"
+	b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -17,6 +23,11 @@ type user struct {
 type LoginUser struct {
 	ID       string `json:"id"`
 	PASSWORD string `json:"password"`
+}
+
+type FileUrl struct {
+	FILENAME string `json:"filename"`
+	URL      string `json:"fileurl"`
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -42,8 +53,73 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+// PostNotice is Bad Server Fuck You
+func PostNotice(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(200000)
+	fileList := []FileUrl{}
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	author := r.Header.Values("Auth")
+	formdata := r.MultipartForm
+	files := formdata.File["multiplefiles"]
+	content := formdata.Value["content"][0]
+	title := formdata.Value["title"][0]
+	for i, _ := range files {
+		file, err := files[i].Open()
+		defer file.Close()
+		if err != nil {
+			fmt.Fprintln(w, err)
+			return
+		}
+		path := "./static/" + b64.StdEncoding.EncodeToString([]byte(content+title))
+		os.Mkdir(path, 0666)
+		out, err := os.OpenFile(path+"/"+files[i].Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		defer out.Close()
+		fileList = append(fileList, FileUrl{FILENAME: files[i].Filename, URL: "http://localhost:8080/static/" + b64.StdEncoding.EncodeToString([]byte(content+title)) + "/" + files[i].Filename})
+		if err != nil {
+			fmt.Fprintf(w, "Fuck you man")
+		}
+		_, _ = io.Copy(out, file)
+		_, _ = io.WriteString(w, "File Upload Successful!")
+
+	}
+	jsondata, err := json.Marshal(fileList)
+	if err != nil {
+		fmt.Fprintf(w, "Fuck you man")
+	}
+	framework.InsertPOST("board", author[0], "notice", title, content, string(jsondata))
+	if err != nil {
+		fmt.Fprintf(w, "DB ERROR")
+		panic(err)
+	}
+
+}
+
+// GetPost is
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.FormValue("id"))
+	fmt.Printf(r.FormValue("id"))
+	if err != nil {
+		panic(err)
+	}
+	res := framework.GetPOST(id)
+	if res == "" {
+
+	} else {
+		fmt.Fprintf(w, res)
+	}
+
+}
 func main() {
+	framework.InitDB()
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/api/login", login).Methods("POST")
+	api := router.PathPrefix("/api/").Subrouter()
+	api.HandleFunc("/login", login).Methods("POST")
+	api.HandleFunc("/upload", PostNotice).Methods("POST")
+	api.Path("/post").Queries("id", "{[0-9]*?}").HandlerFunc(GetPost).Methods("GET")
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
